55f0ac8266c0745b1d0ccf028c3ef0d9749259e0
JaydeepUniverse <admin@itzgeek.local>
2018-04-06 11:56:53 +0530

    8th added 2 more line in compatible with 4 %r variables

--- 8-more_printing.py
+++ 8-more_printing.py
@@ -5,7 +5,9 @@ print formatter % ("one", "two", "three", "four")
 print formatter % (True, False, True, False)
 print formatter % (formatter, formatter, formatter, formatter)
 print formatter % (
-"I am typing without single quote in it."
-"Now I'm using single qoute."
+"I am typing without single quote in it.",
+"Now I'm using single qoute.",
+"since there are four percentage r in formatter variable",
+"we have to add four lines only"
 )
 

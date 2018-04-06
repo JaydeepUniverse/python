6a85de93d7dc57c2f5b0b3df9cff7d58de2edb19
JaydeepUniverse <admin@itzgeek.local>
2018-04-06 17:32:41 +0530

    9th more printing

---
+++ 9-more_printing.py
@@ -0,0 +1,13 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+print "Here are the days : %s" %days
+print "Here are the months : " months
+
+print """
+With the 3 double quotes we can write as much lines as we can
+this is second line
+3rd
+fourth
+"""
+

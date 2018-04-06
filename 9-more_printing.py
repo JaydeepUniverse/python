78a50d70ccef64714564a78e18cb5dc22ef0901e
JaydeepUniverse <admin@itzgeek.local>
2018-04-06 17:36:16 +0530

    9th more printing forgot to add comma after print and before variable name

--- 9-more_printing.py
+++ 9-more_printing.py
@@ -2,7 +2,7 @@ days = "Mon Tue Wed Thu Fri Sat Sun"
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
 
 print "Here are the days : %s" %days
-print "Here are the months : " months
+print "Here are the months : ", months
 
 print """
 With the 3 double quotes we can write as much lines as we can

34a79fb39d9b68ba4e0122d3214c0b459957881e
JaydeepUniverse <admin@itzgeek.local>
2018-04-06 11:23:55 +0530

    modified autoAddToGit file committing manually now

--- autoAddToGit.sh
+++ autoAddToGit.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-lastCount1=`ll -ltrh|awk '{print $9}'|awk -F - '{print $1}'|grep -v "README\|autoAddToGit"|sort|tail -1`
-lastFile1=`ll -ltrh|grep "$lastCount1-"|awk '{print $9}'`
+lastCount1=`ls -l|awk '{print $9}'|awk -F - '{print $1}'|grep -v "README\|autoAddToGit"|sort|tail -1`
+lastFile1=`ls -l|grep "$lastCount1-"|awk '{print $9}'`
 
 git add $lastFile1
 
 read -p "Enter Git Commit message : " gitCommitMessage
 git commit -m "$gitCommitMessage"
+
+git add autoAddToGit.sh
 
 git status
 

6f90d1e901813a411811a0025732adcaee6797a0
JaydeepUniverse <ecjaydeepsoni@gmail.com>
2019-02-16 23:08:21 +0530

    adding programs

---
+++ functions/basic_2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3.6
+
+import subprocess
+
+def systemKernel():
+    uname = "uname"
+    uname_arg = "-a"
+    print ("\n***************** KERNEL using command >", uname, uname_arg, "*****************", "\n")
+    a = subprocess.call([uname, uname_arg])
+
+def systemDiskspace():
+    diskspace = "df"
+    diskspace_arg = "-h"
+    print ("\n***************** DISKSPACE using command >",diskspace, diskspace_arg, "*****************", "\n")
+    subprocess.call([diskspace, diskspace_arg])
+
+def systemMemory():
+    print ("\n***************** RAM using command > free -m *****************", "\n")
+    subprocess.call("free -m", shell=True)
+
+def main():
+    print ("\nGathering this server's basic informations :")
+    systemKernel()
+    systemDiskspace()
+    systemMemory()
+
+if __name__ == "__main__":
+    main()
